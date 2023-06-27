804d1a72ce772cc516341681425554f911f77f65
Dani Gamss <drgamss@yahoo.com>
2023-06-27 08:45:44 +0300

    lesson 40

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Core.Entities;
 
 namespace Core.Specifications
@@ -5,6 +6,12 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
         public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductBrand);
+            AddInclude(x => x.ProductType);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
