21d1fcf0010de280e6cbd6e84677e90ae91ad0a2
Dani Gamss <drgamss@yahoo.com>
2023-07-08 22:49:30 +0300

    lesson 44

--- API/Dtos/MappingProfiles.cs
+++ API/Dtos/MappingProfiles.cs
@@ -7,7 +7,9 @@ namespace API.Dtos
     {
         public MappingProfiles()
         {
-            CreateMap<Product, ProductToReturnDto>();
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
         }
     }
 }
