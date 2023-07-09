a24e7ea87ac395f242140b9e1b186f694ad408d4
Dani Gamss <drgamss@yahoo.com>
2023-07-09 20:14:05 +0300

    lesson 45

--- API/Dtos/MappingProfiles.cs
+++ API/Dtos/MappingProfiles.cs
@@ -1,3 +1,4 @@
+using API.Helpers;
 using AutoMapper;
 using Core.Entities;
 
@@ -9,7 +10,8 @@ namespace API.Dtos
         {
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
         }
     }
 }
