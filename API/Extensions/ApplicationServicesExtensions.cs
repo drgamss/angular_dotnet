db83395694fc11b873bd7e89aa92efbab2cc7860
Dani Gamss <drgamss@yahoo.com>
2023-08-03 08:57:43 +0300

    end of section 6

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -40,6 +40,11 @@ namespace API.Extensions
                 };
 
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
             return services;
         }
     }
